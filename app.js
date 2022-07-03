fd1475d9941bbb9ff6121fb544db2dafb480c4c9
Jigyasa Gupta <75614134+Jigyasa-G@users.noreply.github.com>
2022-07-04 03:24:00 +0530

    #Level-3 Authentication HASHING

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -22,8 +23,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +42,7 @@ app.post("/register", function(req,res) {
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function (err){
@@ -55,9 +54,11 @@ app.post("/register", function(req,res) {
   });
 });
 
+// console.log(md5("allnumbers"));
+
 app.post("/login", function (req,res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username}, function (err, foundUser){
     if(err)
